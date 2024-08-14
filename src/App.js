21a7fd56b4b370ce5af9ec2e80877bbc8beb9a05
ericbr0wne <ericbrowne@hotmail.com>
2024-08-14 20:23:57 +0200

    exercise 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Greeting from './components/Greeting'; 
+import IncrementButton from './components/IncrementButton';
+import DecrementButton from './components/DecrementButton';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  }
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Greeting name="John"/>
+      <IncrementButton onClick={handleIncrement} />
+      <DecrementButton onClick={handleDecrement} />
+      <p>Count: {count}</p>
     </div>
   );
 }
---
+++ src/components/DecrementButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DecrementButton({ onClick }) {
+    return <button onClick={onClick}>Decrement</button> 
+}
+
+export default DecrementButton;
